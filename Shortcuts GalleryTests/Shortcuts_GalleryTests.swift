d5d3c0fe71039e29d6cf01b8c4e6f578b0511325
Marco Capano <marco.capano@pushapp.me>
2018-07-09 10:42:18 +0200

    Initial Commit

---
+++ Shortcuts GalleryTests/Shortcuts_GalleryTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shortcuts_GalleryTests.swift
+//  Shortcuts GalleryTests
+//
+//  Created by Marco Capano on 09/07/2018.
+//  Copyright © 2018 Marco Capano. All rights reserved.
+//
+
+import XCTest
+@testable import Shortcuts_Gallery
+
+class Shortcuts_GalleryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
